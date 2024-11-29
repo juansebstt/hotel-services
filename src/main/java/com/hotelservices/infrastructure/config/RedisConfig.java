07699098893a7d0b2d2b679902cbd10a7951e5ff
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-11-29 14:27:33 -0500

    Updated project

--- src/main/java/com/hotelservices/infrastructure/config/RedisConfig.java
+++ src/main/java/com/hotelservices/infrastructure/config/RedisConfig.java
@@ -1,7 +1,29 @@
 package com.hotelservices.infrastructure.config;
 
+import com.hotelservices.infrastructure.entity.HotelEntity;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    @Primary
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("cache", 6379);
+        return new LettuceConnectionFactory(config);
+
+    }
+
+    @Bean
+    public ReactiveRedisOperations<String, HotelEntity> redisOperations(ReactiveRedisConnectionFactory factory) {
+        return new ReactiveRedisOperations<>(factory, new HotelEntityRedis ) {
+        }
+    }
+
 }
